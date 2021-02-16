7314cfcbeff29c9b0f73bc10de56cb5df71e678c
filipdasilva <fdasilva04@gmail.com>
2021-02-15 22:59:56 -0600

    Main

---
+++ HarrisonReadThis.py
@@ -0,0 +1,8 @@
+#hey bro thanks for checking into my repository!
+#whenever you make changes if you could leave comments on the file like this
+#it would really help me understand why you are making the changes so i can learn
+x = 'Thanks'
+y = 'Bud'
+huh = x + y
+print(huh)
+
