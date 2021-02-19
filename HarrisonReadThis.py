03b83407bdbc91b3cb8185ebc2138949fec0379a
Harrison <harrisonhayes97@yahoo.com>
2021-02-19 02:58:16 -0600

    Read your file.

--- HarrisonReadThis.py
+++ HarrisonReadThis.py
@@ -9,3 +9,16 @@ QQ = 'I have a question'
 print('QQ')
 #How can i import an array? like if i have the data in an excel file or the resulting dataset from a SQL query? 
 #can i manipulate this data in python?
+
+####### Fo sho brotha.
+#######
+####### I made some comment on your commits.
+####### You can check them directly on github.
+####### Cleaner that way for now as you've seem
+####### To have gotten the hang of git pull etc.
+####### However, it'd be best for you to stil ask
+####### you're direct questions here as you code.
+####### You can leave responses in my comments however.
+#######
+####### And, you're making really good progress.
+
