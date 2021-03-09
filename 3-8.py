cafa74992aeef9aefc516a0a6bc649244c2a7327
filipdasilva <fdasilva04@gmail.com>
2021-03-08 21:11:20 -0600

    Main

---
+++ 3-8.py
@@ -0,0 +1,15 @@
+import urllib.request, urllib.parse, urllib.error
+import twurl
+import json
+
+hey = 5
+
+#how do i import shit like twurl
+
+#take objects and put them together, dont have to create the objects, this is the magical thing
+
+Class = 'Template'
+#class code is the outline and then create an object or an instance
+
+#method is a function that lives inside of a class
+#field/attribute = data/variable inside of class
