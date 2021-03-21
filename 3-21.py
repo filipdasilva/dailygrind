c52dc4f9eb3cac45f807129093005ee9e5631e36
filipdasilva <fdasilva04@gmail.com>
2021-03-21 16:04:22 -0500

    Main

---
+++ 3-21.py
@@ -0,0 +1,16 @@
+class PartyAnimal:
+    x = 0
+    
+    def party(self) :
+        self.x = self.x + 1
+        print("So far",self.x)
+        
+an = PartyAnimal()
+
+an.party()
+an.party()
+an.party()
+
+g = 12
+print(type(g))
+print(dir(g))
